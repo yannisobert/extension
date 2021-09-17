570baf9b2f6398d312f92312c1b064aeb3c058f1
Thibaut <thibaut.francois@edu.devinci.fr>
2021-09-17 12:59:26 +0200

    :hammer: change title page

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ document.getElementsByTagName('body')[0].classList.add('override')
 document.querySelector('p').textContent='Bla bla bla Corona';
 document.querySelector('p').style.color='blue';
 
-document.getElementsByClass('page-Title').textContent='Tableau de bord du meilleur des étudiants !!!';
+document.getElementsByClass('page-title').textContent='Tableau de bord du meilleur des étudiants !!!';
