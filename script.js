e9a732783d784c02ae42c93771e4aa8b953b721e
Thibaut <thibaut.francois@edu.devinci.fr>
2021-09-17 13:04:56 +0200

    :hammer: change height <p>

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ document.getElementsByTagName('body')[0].classList.add('override')
 
 document.querySelector('p').textContent='Bla bla bla Corona';
 document.querySelector('p').style.color='blue';
-document.querySelector('h3').textContent='Tableau de bord du meilleur des étudiants !!!';
+document.querySelector('p').height = "50px";
 
 //document.getElementsByClass('page-title').textContent='Tableau de bord du meilleur des étudiants !!!';
