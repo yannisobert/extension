76bc8b039f61912ce4449911d5f45a9655c240d2
yannisobert <obertyannis@gmail.com>
2021-09-19 15:26:04 +0200

    :hammer: Update text with javascript

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 document.getElementsByTagName('body')[0].classList.add('override')
 
-window.alert("Croisons les doigts avant d'alelr voir les notes ;)");
+window.alert("Croisons les doigts avant d'aller voir les notes ;)");
+
+document.querySelector('p').textContent="Petit bla bla sur le Corona à l'école :";
